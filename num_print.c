796af534f80eaef7c09edf5019ca9567673ca303
Rahma Folorunsho <folorunshorahmat16@gmail.com>
2023-04-25 19:46:33 +0100

    Update File

--- num_print.c
+++ num_print.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * dec_print - decimal (base 10) printing function
+ * @args: variable argument list
+ * @count: pointer to integer to store the count of characters printed
+ * d - decimal (int) parameter passed
+ */
+
 void dec_print(va_list args, int *count)
 {
     int d, i, j, d_len;
