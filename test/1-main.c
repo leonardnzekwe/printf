5a4947ad42d9c6fd804c78421866e3bdde3fa3f6
Rahma Folorunsho <folorunshorahmat16@gmail.com>
2023-04-25 21:47:04 +0100

    Update File

--- test/1-main.c
+++ test/1-main.c
@@ -18,6 +18,12 @@ int main(void)
     printf("Length:[%d, %i]\n", len2, len2);
     _printf("Negative:[%d]\n", -762534);
     printf("Negative:[%d]\n", -762534);
+    _printf("Positive:[%d]\n", 962534);
+    printf("Positive:[%d]\n", 962534);
+    _printf("Int Positive:[%i]\n", 242962534);
+    printf("Int Positive:[%i]\n", 242962534);
+    _printf("Int Negative:[%i]\n", -8962534);
+    printf("Int Negative:[%i]\n", -8962534);
     _printf("Character:[%c]\n", 'H');
     printf("Character:[%c]\n", 'H');
     _printf("String:[%s]\n", "I am a string !");
@@ -28,5 +34,23 @@ int main(void)
     printf("Len:[%d]\n", len2);
     _printf("Unknown:[%r]\n");
     printf("Unknown:[%r]\n");
+    _printf("Negative String:[%d]\n", "-leo");
+    printf("Negative String:[%d]\n", "-leo");
+    _printf("Positive String:[%d]\n", "ra+y");
+    printf("Positive String:[%d]\n", "ra+y");
+    _printf("----------------\n");
+    _printf("Negative String:[%d]\n", "-leo");
+    printf("Negative String:[%d]\n", "-leo");
+    _printf("Positive String:[%d]\n", "ra+y");
+    printf("Positive String:[%d]\n", "ra+y");
+    _printf("----------------\n");
+    _printf("Zero:[%d]\n", 0);
+    printf("Zero: [%d]\n", 0);
+    _printf("Zero:[%i]\n", 0);
+    printf("Zero:[%i]\n", 0);
+    _printf("no arg:[%d]\n");
+    printf("no arg: [%d]\n");
+    _printf("no arg:[%i]\n");
+    printf("no arg:[%i]\n");
     return (0);
 }
