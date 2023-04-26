0d7bd87040cd053f36e71ddfcce6ff8e1958a4f2
Leonard Nzekwe <nzekweleo@gmail.com>
2023-04-26 09:40:14 +0100

    Updated File, print_func.c

--- print_func.c
+++ print_func.c
@@ -56,7 +56,6 @@ int print_fmt(const char *format, va_list args, int *count, int *num_args)
 			/* check for standalone % and if % is the last character*/
 			if (format[i] == '\0')
 				{ _putchar(format[i - 2]);
-					(*count)++;
 					return (-1); }
 			for (j = 0; fmt_specs[j].fmt_sign != '\0'; j++)
 			{
